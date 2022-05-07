0eb5a0579f9151f025d42e1247aa6e726a04e2c0
Shachar Shemesh <shachar@shemesh.biz>
2022-05-07 06:42:48 +0300

    Skeleton

---
+++ Apple2plus.srcs/sources_1/new/apple2p.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Some Assembly Required
+// Engineer: Shachar Shemesh
+// 
+// Create Date: 05/07/2022 06:40:20 AM
+// Design Name: apple2plus
+// Module Name: apple2p
+// Project Name: compusar
+// Target Devices: Spartan-7 15
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module apple2p(
+    input clock,
+    output power_led,
+    output activity_led,
+    output uart_send,
+    input uart_recv
+    );
+endmodule
